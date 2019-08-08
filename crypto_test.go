c1a7abc7b9ebf6be9a943145a4685948bca23ae9
Jakub Sztandera <kubuxu@protonmail.ch>
2019-08-08 19:30:57 +0200

    Fix benchamrks
    License: MIT
    Signed-off-by: Jakub Sztandera <kubuxu@protonmail.ch>

--- crypto_test.go
+++ crypto_test.go
@@ -9,24 +9,25 @@ import (
 	"golang.org/x/crypto/chacha20poly1305"
 )
 
-func BenchmarkCacha20Poly1305(b *testing.B) {
+func BenchmarkChacha20Poly1305(b *testing.B) {
 	key1 := make([]byte, chacha20poly1305.KeySize)
-	key2 := make([]byte, chacha20poly1305.KeySize)
-	nonce1 := make([]byte, chacha20poly1305.NonceSize)
 	rand.Read(key1)
-	rand.Read(key2)
-	rand.Read(nonce1)
 
 	aead, err := chacha20poly1305.New(key1)
 	if err != nil {
 		b.Fatal(err)
 	}
+
+	nonce1 := make([]byte, aead.NonceSize())
+	rand.Read(nonce1)
+
 	data := make([]byte, 1<<19)
 	rand.Read(data)
+
 	b.SetBytes(1 << 19)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		aead.Seal(key2, nonce1, data, []byte{})
+		aead.Seal(data[:0], nonce1, data, []byte{})
 	}
 
 }
@@ -41,11 +42,7 @@ func BenchmarkAES256GCM(b *testing.B) {
 
 func benchmarkAESX(b *testing.B, keysize int) {
 	key1 := make([]byte, keysize)
-	key2 := make([]byte, keysize)
-	nonce1 := make([]byte, chacha20poly1305.NonceSize)
 	rand.Read(key1)
-	rand.Read(key2)
-	rand.Read(nonce1)
 
 	aes, err := aes.NewCipher(key1)
 	if err != nil {
@@ -56,12 +53,17 @@ func benchmarkAESX(b *testing.B, keysize int) {
 	if err != nil {
 		b.Fatal(err)
 	}
+
+	nonce1 := make([]byte, aead.NonceSize())
+	rand.Read(nonce1)
+
 	data := make([]byte, 1<<19)
 	rand.Read(data)
+
 	b.SetBytes(1 << 19)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		aead.Seal(key2, nonce1, data, []byte{})
+		aead.Seal(data[:0], nonce1, data, []byte{})
 	}
 
 }
